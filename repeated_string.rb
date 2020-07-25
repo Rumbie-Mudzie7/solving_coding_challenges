9fd81f903d71f952b583a502dfccf90edace6bb3
rumbie <rumbiemudzie@gmail.com>
2020-07-25 10:26:42 +0200

    Implement solution through trial and error

--- repeated_string.rb
+++ repeated_string.rb
@@ -1,16 +1,24 @@
-def repeated_string(str,num)
-count = 0
+def repeated_string(s,n)
+    
+    num_of_multiples = n / s.length
+    count = 0
+    remainder = n % s.length
+    extra_letter = 0
+    result_a_count = 0
+
 i = 0
-while i <= num
-
-if str[i] == "a".downcase 
-# return num 
-count += 1
-end
-
+while i < s.length
+   
+    if s[i] == "a"
+        count += 1
+    end
+    if remainder > i   
+        extra_letter += 1
+    end
 i += 1
 end
-count
+result_a_count = num_of_multiples * count + extra_letter
 end
 
-p repeated_string("abaaa", 10)
+puts repeated_string("abcac", 10)
+
